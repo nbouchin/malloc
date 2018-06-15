bd6f8bec53711e32b0433f999ab572f64513ffdf
Nicolas BOUCHINET <nbouchin@student.42.fr>
2018-06-15 18:03:27 +0200

    Start: tiny allocation memory

--- srcs/malloc.c
+++ srcs/malloc.c
@@ -12,40 +12,64 @@
 
 #include "../includes/libft_malloc.h"
 
-void	free_list_itter(t_block *free_list, void (*f)(t_block *elem))
+int			new_zone(int index)
 {
-	while (free_list)
-	{
-		f(free_list);
-		free_list = free_list->nxt;
-	}
+	if (!g_zone[index].page)
+		return (1);
+	else
+		return (0);
 }
 
-void		init_zone(size_t index, size_t sze)
+void	init_zone(size_t index, size_t sze)
 {
 	g_zone[index].page = mmap(NULL, sze, PROT_WRITE | PROT_READ,
 			MAP_ANON | MAP_PRIVATE, -1, 0);
 	g_zone[index].free_list = ft_memcpy(g_zone[index].free_list,
 			g_zone[index].page, sze);
-	g_zone[index].free_list->nxt_free = NULL;
 	g_zone[index].free_list->sze = sze;
 	g_zone[index].free_list->nxt = NULL;
+	g_zone[index].free_list->nxt_free = NULL;
 	g_zone[index].total_sze = sze;
 	g_zone[index].page->sze = sze;
 	g_zone[index].page->nxt = NULL;
 }
 
-void		get_alloc_sze(int sze)
+void	init_big_zone(size_t index, size_t sze)
 {
-	if (sze <= TINY)
-		init_zone(N, 0);
-	else if (sze > TINY && sze <= SMALL)
-		init_zone(M, 1);
+	g_zone[index].total_sze = sze;
+	g_zone[index].free_list = NULL;
+	g_zone[index].page = mmap(NULL, sze, PROT_WRITE | PROT_READ,
+			MAP_ANON | MAP_PRIVATE, -1, 0);
+	g_zone[index].page->nxt = NULL;
+	g_zone[index].page->sze = 0;
+}
+
+void	new_block(size_t index)
+{
+	t_block *swap;
+
+	swap = g_zone[index].free_list;
+	while (swap)
+		swap = swap->nxt;
+}
+
+void		check_zone(int sze)
+{
+	if (sze <= TINY && new_zone(0))
+		init_zone(0, N);
+	else if (sze > TINY && sze <= SMALL && new_zone(1))
+		init_zone(1, M);
 	else if (sze > SMALL)
-		init_zone(sze, 2);
+		init_big_zone(2, sze);
+	else if (sze <= TINY)
+		new_block(0);
+	else if (sze > TINY && sze <= SMALL)
+		new_block(1);
+	else if (sze > SMALL)
+		new_block(2);
 }
 
 void		*malloc(size_t sze)
 {
-	get_alloc_sze(sze);
+	check_zone(sze);
 }
