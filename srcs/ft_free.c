8b8af218bd5071bcb705d4cbc99012d5af6acdc5
Nicolas BOUCHINET <nbouchin@student.42.fr>
2018-06-27 16:51:01 +0200

    Update: debug next free size

--- srcs/ft_free.c
+++ srcs/ft_free.c
@@ -6,7 +6,7 @@
 /*   By: nbouchin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/13 09:13:59 by nbouchin          #+#    #+#             */
-/*   Updated: 2018/06/27 14:11:44 by nbouchin         ###   ########.fr       */
+/*   Updated: 2018/06/27 16:49:18 by nbouchin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,12 +65,14 @@ void		large_free(void *ptr)
 	p = NULL;
 	page = g_zone[2].page;
 	p = (((t_block *)(page + 1)) + 1);
-	while (page)
+	while (page->nxt)
 	{
 		if (((t_block *)(page + 1) + 1) == ptr)
 		{
-			p = (((t_block *)(page + 1) + 1));
+			ft_putendl("SALUT");
+			p = (t_block *)(page + 1);
 			delete_page(p, &page, &prev, 2);
+			break ;
 		}
 		prev = page;
 		page = page->nxt;
--- tests/test2.c
+++ tests/test2.c
@@ -12,148 +12,131 @@ int main()
 
 	i = 0;
 
-	while (i < 1024)
+//	ft_putendl("TINY MALLOC TEST");
+//	i = 0;
+//	while (i < 5000)
+//	{
+//		addr = (char*)ft_malloc(450);
+//		addr[0] = 42; addr1 = (char*)ft_malloc(450);
+//		addr1[0] = 42;
+//		addr2 = (char*)ft_malloc(450);
+//		addr2[0] = 42;
+//		addr3 = (char*)ft_malloc(450);
+//		addr3[0] = 42;
+//		addr4 = (char*)ft_malloc(450);
+//		addr4[0] = 42;
+//		ft_free(addr2);
+//		ft_free(addr4);
+//		ft_free(addr3);
+//		ft_free(addr1);
+//		ft_free(addr);
+//		i++;
+//	}
+//	ft_putendl("SMALL MALLOC TEST");
+//	i = 0;
+//	while (i < 5000)
+//	{
+//		addr = (char*)ft_malloc(1200);
+//		addr[0] = 42;
+//		addr1 = (char*)ft_malloc(1200);
+//		addr1[0] = 42;
+//		addr2 = (char*)ft_malloc(1200);
+//		addr2[0] = 42;
+//		addr3 = (char*)ft_malloc(1200);
+//		addr3[0] = 42;
+//		addr4 = (char*)ft_malloc(1200);
+//		addr4[0] = 42;
+//		ft_free(addr2);
+//		ft_free(addr4);
+//		ft_free(addr3);
+//		ft_free(addr1);
+//		ft_free(addr);
+//		i++;
+//	}
+//	ft_putendl("LARGE MALLOC TEST");
+//	i = 0;
+	//while (i < 5000)
+	//{
+	//	addr = (char*)ft_malloc(127450);
+	//	addr[0] = 42;
+	//	addr1 = (char*)ft_malloc(127450);
+	//	addr1[0] = 42;
+	//	addr2 = (char*)ft_malloc(127450);
+	//	addr2[0] = 42;
+	//	addr3 = (char*)ft_malloc(127450);
+	//	addr3[0] = 42;
+	//	addr4 = (char*)ft_malloc(127450);
+	//	addr4[0] = 42;
+	//	ft_free(addr2);
+	//	ft_free(addr4);
+	//	ft_free(addr3);
+	//	ft_free(addr1);
+	//	ft_free(addr);
+	//	i++;
+	//}
+//	ft_putendl("TINY MALLOC TEST");
+//	i = 0;
+//	while (i < 5000)
+//	{
+//		addr = (char*)ft_malloc(450);
+//		addr[0] = 42;
+//		addr1 = (char*)ft_malloc(450);
+//		addr1[0] = 42;
+//		addr2 = (char*)ft_malloc(450);
+//		addr2[0] = 42;
+//		addr3 = (char*)ft_malloc(450);
+//		addr3[0] = 42;
+//		addr4 = (char*)ft_malloc(450);
+//		addr4[0] = 42;
+//		ft_free(addr);
+//		ft_free(addr1);
+//		ft_free(addr2);
+//		ft_free(addr3);
+//		ft_free(addr4);
+//		i++;
+//	}
+//	ft_putendl("SMALL MALLOC TEST");
+//	i = 0;
+//	while (i < 5000)
+//	{
+//		addr = (char*)ft_malloc(1200);
+//		addr[0] = 42;
+//		addr1 = (char*)ft_malloc(1200);
+//		addr1[0] = 42;
+//		addr2 = (char*)ft_malloc(1200);
+//		addr2[0] = 42;
+//		addr3 = (char*)ft_malloc(1200);
+//		addr3[0] = 42;
+//		addr4 = (char*)ft_malloc(1200);
+//		addr4[0] = 42;
+//		ft_free(addr);
+//		ft_free(addr1);
+//		ft_free(addr2);
+//		ft_free(addr3);
+//		ft_free(addr4);
+//		i++;
+//	}
+	ft_putendl("LARGE MALLOC TEST");
+	i = 0;
+	while (i < 2) 
 	{
-		addr = (char*)ft_malloc(450);
+		addr = (char*)ft_malloc(127450);
 		addr[0] = 42;
-		addr1 = (char*)ft_malloc(450);
+		addr1 = (char*)ft_malloc(127450);
 		addr1[0] = 42;
-		addr2 = (char*)ft_malloc(450);
+		addr2 = (char*)ft_malloc(127450);
 		addr2[0] = 42;
-		addr3 = (char*)ft_malloc(450);
+		addr3 = (char*)ft_malloc(127450);
 		addr3[0] = 42;
-		addr4 = (char*)ft_malloc(450);
+		addr4 = (char*)ft_malloc(127450);
 		addr4[0] = 42;
+		ft_free(addr);
+		ft_free(addr1);
+		ft_free(addr2);
+		ft_free(addr3);
+		ft_free(addr4);
 		i++;
 	}
-	//	ft_putendl("TINY MALLOC TEST");
-	//	i = 0;
-	//	while (i < 2000)
-	//	{
-	//		addr = (char*)ft_malloc(450);
-	//		addr[0] = 42;
-	//		addr1 = (char*)ft_malloc(450);
-	//		addr1[0] = 42;
-	//		addr2 = (char*)ft_malloc(450);
-	//		addr2[0] = 42;
-	//		addr3 = (char*)ft_malloc(450);
-	//		addr3[0] = 42;
-	//		addr4 = (char*)ft_malloc(450);
-	//		addr4[0] = 42;
-	////		ft_free(addr2);
-	////		ft_free(addr4);
-	////		ft_free(addr3);
-	////		ft_free(addr1);
-	////		ft_free(addr);
-	//		i++;
-	//	}
-	//
-	//	ft_putendl("SMALL MALLOC TEST");
-	//	i = 0;
-	//	while (i < 2000)
-	//	{
-	//		addr = (char*)ft_malloc(1200);
-	//		addr[0] = 42;
-	//		addr1 = (char*)ft_malloc(1200);
-	//		addr1[0] = 42;
-	//		addr2 = (char*)ft_malloc(1200);
-	//		addr2[0] = 42;
-	//		addr3 = (char*)ft_malloc(1200);
-	//		addr3[0] = 42;
-	//		addr4 = (char*)ft_malloc(1200);
-	//		addr4[0] = 42;
-	////		ft_free(addr2);
-	////		ft_free(addr4);
-	////		ft_free(addr3);
-	////		ft_free(addr1);
-	////		ft_free(addr);
-	//		i++;
-	//	}
-	//	ft_putendl("LARGE MALLOC TEST");
-	//	i = 0;
-	//	while (i < 2000)
-	//	{
-	//		addr = (char*)ft_malloc(127450);
-	//		addr[0] = 42;
-	//		addr1 = (char*)ft_malloc(127450);
-	//		addr1[0] = 42;
-	//		addr2 = (char*)ft_malloc(127450);
-	//		addr2[0] = 42;
-	//		addr3 = (char*)ft_malloc(127450);
-	//		addr3[0] = 42;
-	//		addr4 = (char*)ft_malloc(127450);
-	//		addr4[0] = 42;
-	////		ft_free(addr2);
-	////		ft_free(addr4);
-	////		ft_free(addr3);
-	////		ft_free(addr1);
-	////		ft_free(addr);
-	//		i++;
-	//	}
-	//	ft_putendl("TINY MALLOC TEST");
-	//	i = 0;
-	//	while (i < 2000)
-	//	{
-	//		addr = (char*)ft_malloc(450);
-	//		addr[0] = 42;
-	//		addr1 = (char*)ft_malloc(450);
-	//		addr1[0] = 42;
-	//		addr2 = (char*)ft_malloc(450);
-	//		addr2[0] = 42;
-	//		addr3 = (char*)ft_malloc(450);
-	//		addr3[0] = 42;
-	//		addr4 = (char*)ft_malloc(450);
-	//		addr4[0] = 42;
-	////		ft_free(addr);
-	////		ft_free(addr1);
-	////		ft_free(addr2);
-	////		ft_free(addr3);
-	////		ft_free(addr4);
-	//		i++;
-	//	}
-	//
-	//	ft_putendl("SMALL MALLOC TEST");
-	//	i = 0;
-	//	while (i < 2000)
-	//	{
-	//		addr = (char*)ft_malloc(1200);
-	//		addr[0] = 42;
-	//		addr1 = (char*)ft_malloc(1200);
-	//		addr1[0] = 42;
-	//		addr2 = (char*)ft_malloc(1200);
-	//		addr2[0] = 42;
-	//		addr3 = (char*)ft_malloc(1200);
-	//		addr3[0] = 42;
-	//		addr4 = (char*)ft_malloc(1200);
-	//		addr4[0] = 42;
-	////		ft_free(addr);
-	////		ft_free(addr1);
-	////		ft_free(addr2);
-	////		ft_free(addr3);
-	////		ft_free(addr4);
-	//		i++;
-	//	}
-	//	ft_putendl("LARGE MALLOC TEST");
-	//	i = 0;
-	//	while (i < 2000)
-	//	{
-	//		addr = (char*)ft_malloc(127450);
-	//		addr[0] = 42;
-	//		addr1 = (char*)ft_malloc(127450);
-	//		addr1[0] = 42;
-	//		addr2 = (char*)ft_malloc(127450);
-	//		addr2[0] = 42;
-	//		addr3 = (char*)ft_malloc(127450);
-	//		addr3[0] = 42;
-	//		addr4 = (char*)ft_malloc(127450);
-	//		addr4[0] = 42;
-	////		ft_free(addr);
-	////		ft_free(addr1);
-	////		ft_free(addr2);
-	////		ft_free(addr3);
-	////		ft_free(addr4);
-	//		i++;
-	//	}
 	ft_test();
 	return (0);
 } 
