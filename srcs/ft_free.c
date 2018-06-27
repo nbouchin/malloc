587dd77686ee3427605fdb6c2f884fe3496aa990
Nicolas BOUCHINET <nbouchin@student.42.fr>
2018-06-27 17:24:33 +0200

    BUG: last large page free dont work

--- srcs/ft_free.c
+++ srcs/ft_free.c
@@ -6,7 +6,7 @@
 /*   By: nbouchin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/13 09:13:59 by nbouchin          #+#    #+#             */
-/*   Updated: 2018/06/27 17:03:37 by nbouchin         ###   ########.fr       */
+/*   Updated: 2018/06/27 17:23:48 by nbouchin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,10 +65,8 @@ void		large_free(void *ptr)
 	p = NULL;
 	page = g_zone[2].page;
 	p = (((t_block *)(page + 1)) + 1);
-	ft_putendl("NEW CALL");
 	while (page->nxt)
 	{
-		ft_putendl("SALUT");
 		if (((t_block *)(page + 1) + 1) == ptr)
 		{
 			p = (t_block *)(page + 1);
