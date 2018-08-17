4e0dabbf3ffc5f1eaf069b3890bf29e81cfb5c1a
Nicolas BOUCHINET <nbouchin@student.42.fr>
2018-08-17 16:55:42 +0200

    Update: new malloc creation

--- srcs/malloc.c
+++ srcs/malloc.c
@@ -5,6 +5,9 @@ t_zone g_zone[3] = {{0, NULL, NULL},
 					{0, NULL, NULL},
 					{0, NULL, NULL}};
 
+/*
+**	Create a new generic page and init the first free node.
+*/
 
 t_page		*new_page(size_t page_size)
 {
@@ -22,15 +25,20 @@ t_page		*new_page(size_t page_size)
 	return (page);
 }
 
-//t_block		*new_block(size_t block_size, int status)
-//{
-//	t_block	*p;
-//
-//	p = NULL;
-//	p->size = block_size;
-//	p->is_free = status;
-//	return (p);
-//}
+/*
+**	Fill a block informations and set his status.
+*/
+
+void		fill_block(t_block **p, t_block *next, size_t block_size, int status)
+{
+	(*p)->size = block_size;
+	(*p)->is_free = status;
+	(*p)->nxt = next;
+}
+
+/*
+**	Create a new memory zone according to the asked zone type.
+*/
 
 void	new_zone(size_t zone_size, int zone_type)
 {
@@ -47,7 +55,11 @@ void	new_zone(size_t zone_size, int zone_type)
 	}
 }
 
-t_block	*place_cursor(t_page *page)
+/*
+**	Run through pages block to get a free block.
+*/
+
+t_block	*search_free_block(t_page *page, size_t alloc_size)
 {
 	t_block *p;
 
@@ -57,7 +69,7 @@ t_block	*place_cursor(t_page *page)
 		p = (t_block *)(page + 1);
 		while (p)
 		{
-			if (p->is_free)
+			if (p->is_free && alloc_size <= p->size)
 				return (p);
 			p = p->nxt;
 		}
@@ -66,31 +78,67 @@ t_block	*place_cursor(t_page *page)
 	return (NULL);
 }
 
+/*
+**	Compute the allocation offset and return new allocation size.
+*/
+
 size_t	get_offset(size_t alloc_size, int offset)
 {
 	return (alloc_size = (alloc_size + (offset - 1)) & ~(offset - 1));
 }
 
-void	*tiny_allocation(alloc_size)
-{
-	t_block	*cursor;
+/*
+**	Relink a new non-free block, with the old free block and the old free block next block.
+*/
 
-	new_zone(alloc_size, 0);
-	cursor = place_cursor(g_zone[0].page);
-	if (cursor)
+t_block	*relink_block(t_block *block, size_t alloc_size, size_t offset)
+{
+	t_block *backup;
+	size_t	old_size;
+
+	if (block->nxt)
 	{
-		cursor->size = alloc_size;
-		cursor->is_free = 0;
-		cursor->nxt = (t_block *)(char *)((cursor) + 1) + get_offset(alloc_size, 16);
-		cursor->nxt->is_free = 1;
+		old_size = block->size;
+		backup = block->nxt;
+		fill_block(&block, block + get_offset(block->size, offset), alloc_size, 0);
+		fill_block(&block->nxt, backup, get_offset(old_size, offset)
+		- get_offset(block->size, offset), 1);
+		return (block);
 	}
 	else
 	{
-		
+		old_size = block->size;
+		fill_block(&block, block + get_offset(block->size, offset), alloc_size, 0);
+		fill_block(&block->nxt, NULL, get_offset(old_size, offset) - get_offset(block->size, offset), 1);
+		return (block);
 	}
-		
-	return (cursor);
 }
+
+/*
+**	Tiny allocation general runtime.
+*/
+
+void	*tiny_allocation(alloc_size)
+{
+	t_block	*block;
+	t_page	*page;
+
+	page = NULL;
+	new_zone(alloc_size, 0);
+	block = search_free_block(g_zone[0].page, alloc_size);
+	if (block)
+		block = relink_block(block, alloc_size, 16);
+	else
+	{
+		page = new_page(TINY);
+		block = ((t_block *)page) + 1;
+	}
+	return (block);
+}
+
+/*
+**	Run allocation runtime according the allocation size request.
+*/
 
 void	*ft_malloc(size_t alloc_size)
 {
